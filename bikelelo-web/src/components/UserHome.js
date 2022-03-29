e6fb8122620735eefb3459610a9ff1639bfe0372
Siddhesh Themdeo <themdeo.siddhesh@gmail.com>
2022-03-29 16:09:31 +0530

    React App Files without Node-Modules Folder

---
+++ bikelelo-web/src/components/UserHome.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class UserHome extends Component {
+    render() {
+        return (
+            <div>
+                <h1>User Home Page</h1>
+            </div>
+        );
+    }
+}
+
+export default UserHome;
