a310730150ff2d88793ca1334d13fd6bdd479fab
Siddhesh Themdeo <themdeo.siddhesh@gmail.com>
2022-04-14 10:04:19 +0530

    Delete bikelelo-web directory

--- bikelelo-web/src/components/store.js
+++
@@ -1,7 +0,0 @@
-import { createStore } from "redux"
-import reducer from "./reducer";
-
-const initialSt = {loggedin: false};
-const mystore = createStore(reducer, initialSt);
-
-export default mystore;
